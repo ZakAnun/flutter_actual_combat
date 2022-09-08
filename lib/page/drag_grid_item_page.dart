9de2a568be650a18d8e02375423bddb08c16a933
zak <linhenji@163.com>
2022-09-08 14:43:32 +0800

    feat: drag list official demo

--- lib/page/drag_grid_item_page.dart
+++ lib/page/drag_grid_item_page.dart
@@ -9,12 +9,37 @@ class DragGridItemPage extends StatefulWidget {
 }
 
 class _DragGridItemPageState extends State<DragGridItemPage> {
+
+  final List<int> _items = List<int>.generate(50, (int index) => index);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      height: double.infinity,
-      width: double.infinity,
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('ReorderableListView Sample')),
+      body: ReorderableListView(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        children: <Widget>[
+          for (int index = 0; index < _items.length; index += 1)
+            ListTile(
+              key: Key('$index'),
+              tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+              title: Text('Item ${_items[index]}'),
+            ),
+        ],
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final int item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+      ),
     );
   }
 
