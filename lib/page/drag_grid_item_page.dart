382257b51d84d492298187440d059dabf045883b
zak <linhenji@163.com>
2022-09-08 13:48:52 +0800

    feat: 新增 drag item page

---
+++ lib/page/drag_grid_item_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DragGridItemPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _DragGridItemPageState();
+  }
+}
+
+class _DragGridItemPageState extends State<DragGridItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      height: double.infinity,
+      width: double.infinity,
+    );
+  }
+
+}
